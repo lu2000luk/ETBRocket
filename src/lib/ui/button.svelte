dd8156f7674f06494ead03bd95dad0b6d96fa629
lu2000luk <lu2000luk@gmail.com>
2024-03-24 20:18:28 +0100

    Did some stuff
    Added lucide svelte, made some db examples and started woriking on the btn component

---
+++ src/lib/ui/button.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let click = () => {};
+  export let bg = "primary";
+  export let text = "text"
+</script>
+
+<button on:click={click} class="rounded py-1 px-3 m-1" style="background: var(--{bg}); color: var(--{text});">
+    <slot />
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Button from "$lib/ui/button.svelte"
+</script>
+
+<Button click={() => {console.log("Hi")}}>
+    Hello
+</Button>
