747d24e1bb8bbd85f6a71a97f076dcd44159297b
lu2000luk <lu2000luk@gmail.com>
2024-03-24 21:50:52 +0100

    Added sidebar and BitsUI

---
+++ src/lib/components/sideicon.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let click = () => {};
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click={click} class="p-3 m-1 hover:bg-background-overlay rounded-md transition-all cursor-pointer">
+    <slot />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
   import "$lib/app.css";
+  import Mod from "lucide-svelte/icons/blocks"
+  import Loaders from "lucide-svelte/icons/unplug"
+  import SideIcon from "$lib/components/sideicon.svelte"
+  import Rocket from "lucide-svelte/icons/rocket"
 </script>
 
-<slot />
+<div class="flex">
+  <div class="sidebar w-20 bg-background-alt flex flex-col content-center items-center">
+    <div class="p-3 m-1"><Rocket /></div>
+    <SideIcon><Mod /></SideIcon>
+    <SideIcon><Loaders /></SideIcon>
+  </div>
+  <div>
+    <slot />
+  </div>
+</div>
