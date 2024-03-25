a7f29498d26bc4541d3c935213f78cebbf7a2e0e
lu2000luk <lu2000luk@gmail.com>
2024-03-25 17:41:36 +0100

    Added mod missing pic and loader

---
+++ src/lib/components/loading.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let size = 20;
+    export let props = {};
+    import Loader from "lucide-svelte/icons/loader-circle"
+</script>
+
+<style>
+    .loader {
+        animation: rotate 0.5s normal ease-in-out infinite;
+    }
+
+    @keyframes rotate {
+        0% {
+            rotate: 0deg;
+        }
+        100% {
+            rotate: 360deg;
+        }
+    }
+</style>
+
+<div class="loader mr-1 origin-center">
+    <Loader size={size} {...props} />
+</div>
--- src/lib/components/modcard.svelte
+++ src/lib/components/modcard.svelte
@@ -1,13 +1,16 @@
 <script>
-    export let name = "Example";
-    export let description = "Example description";
-    export let author = "lu2000luk";
-    export let cover = "https://unsplash.it/120/120";
-    export let loader = "Interpose"
+    export let name = "Missing title";
+    export let description = "";
+    export let author = "Unknown";
+    export let cover = "../../../../no_image.png";
+    export let loader = "Unknown"
 
     import Img from "$lib/ui/image.svelte";
     import Button from "$lib/ui/button.svelte";
     import Download from "lucide-svelte/icons/download";
+    import Loading from "$lib/components/loading.svelte"
+
+    let loading = false;
 </script>
 
 <div class="modcard flex h-30 w-50 bg-background-alt shadow-sm hover:shadow-md rounded-md p-2 m-2">
@@ -21,9 +24,15 @@
         <div class="download flex items-end">
             <div class="flex items-center">
                 <p class="px-1">Made by {author}</p>
-                <Button>
-                    <Download size={20} class="pr-1" /> Download
-                </Button>
+                {#if loading}
+                    <Button disabled bg="primary-dark">
+                        <Loading /> Downloading...
+                    </Button>
+                {:else}
+                    <Button click={() => {loading = true;}} >
+                        <Download size={20} class="pr-1" /> Download
+                    </Button>
+                {/if}
             </div>
         </div>
     </div>
