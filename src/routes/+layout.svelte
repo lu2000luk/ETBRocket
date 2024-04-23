b24e80d4a7e1e44b2cca906b46128d7313b2be1d
lu2000luk <lu2000luk@gmail.com>
2024-04-23 19:37:34 +0200

    Switched to nexus sso
    THIS IS BUGGY AND NOT USER FRIENDLY. This code needs to be reworked on before being pushed to production

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,9 +20,49 @@
   import { steamPath } from "$lib/settings"
 
   if (!$NexusConfig.apiKey) {
-    dialogOpened.set(true);
-  } else {
-    dialogOpened.set(false);
+    nexusConnect()
+  }
+
+  const application_slug = "etbrocket"
+
+  function nexusConnect() {
+    let socket = new WebSocket("wss://sso.nexusmods.com");
+
+    socket.onopen = function (event) {
+      console.log("CONNECTION TO SSO OPENED");
+
+      var uuid = $NexusConfig.uuid;
+      var token = $NexusConfig.token;
+
+      const data = {
+              id: uuid,
+              token: token,
+              protocol: 2
+          };
+
+      socket.send(JSON.stringify(data));
+
+      console.log("https://www.nexusmods.com/sso?id="+uuid+"&application="+application_slug)
+
+      open("https://www.nexusmods.com/sso?id="+uuid+"&application="+application_slug);
+    }
+
+    socket.onmessage = function(event) {
+      let response = JSON.parse(event.data);
+
+      if (response && response.success) {
+        if (response.data.hasOwnProperty('connection_token')) {
+          NexusConfig.set({...$NexusConfig, token: response.data.connection_token})
+        }
+
+        if (response.data.hasOwnProperty('api_key')) {
+            console.log("API Key Received: " + response.data.api_key);
+            NexusConfig.set({...$NexusConfig, apiKey: response.data.api_key})
+        }
+      } else {
+        alert("Error while logging in!")
+      }
+    }
   }
 
   if (!$steamPath) {
@@ -30,27 +70,6 @@
   }
 
   let apiKeyInput = $NexusConfig.apiKey ? $NexusConfig.apiKey : "";
-
-  async function setKey(key=apiKeyInput) {
-    console.log("Checking key... ")
-
-    const httpclient = await getClient();
-    const checker = await httpclient.get('https://api.nexusmods.com/v1/users/validate.json', {
-      responseType: ResponseType.JSON,
-      headers: {
-        apiKey: key
-      }
-    });
-
-    if (checker.status !== 200) {
-      alert("Invalid key! Make sure you copied the personal API Key.")
-    } else {
-      NexusConfig.set({...$NexusConfig, apiKey: key})
-      alert("Key saved!")
-      dialogOpened.set(false);
-      location.reload()
-    }
-  }
 </script>
 
 <div class="flex h-screen">
@@ -104,57 +123,3 @@
     <slot />
   </div>
 </div>
-
-<Dialog.Root bind:open={$dialogOpened}>
-  <Dialog.Portal>
-    <Dialog.Overlay
-      class="fixed inset-0 z-50 bg-black/80"
-    />
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <Dialog.Content
-      class="fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] rounded-md border-none bg-background p-5 shadow-popover outline-none sm:max-w-[490px] md:w-full"
-    >
-      <Dialog.Title
-        class="flex w-full items-center justify-center text-lg font-semibold tracking-tight"
-        >Add Nexus API key</Dialog.Title
-      >
-      <Separator.Root class="-mx-5 mb-6 mt-5 block h-px bg-muted" />
-      <!-- svelte-ignore missing-declaration -->
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <!-- svelte-ignore a11y-missing-attribute -->
-      <!-- svelte-ignore a11y-missing-attribute -->
-      <Dialog.Description class="text-sm text-foreground-alt">
-        <!-- svelte-ignore missing-declaration -->
-        Adding a Nexus API key will make you able to download mods such as Interpose directly from this launcher.<br>Get a key 
-        <a class="underline text-blue-600 cursor-pointer" on:click={() => open("https://next.nexusmods.com/settings/api-keys")}
-         >in this page</a>, at the bottom of it you'll find a button to get a personal API Key.<br>
-         <i>Your key will be stored locally and wont be shared with no one</i>
-      </Dialog.Description>
-      <div class="flex flex-col items-start gap-1 pb-11 pt-7">
-        <Label.Root for="apiKey" class="text-sm font-medium">Nexus Personal API Key:</Label.Root
-        >
-        <div class="relative w-full">
-          <input
-            id="apiKey"
-            class="h-input w-full border-none bg-background-alt rounded focus:outline-none p-2"
-            placeholder="API Key"
-            type="password"
-            autocomplete="off"
-            bind:value={apiKeyInput}
-          />
-        </div>
-      </div>
-      <div class="flex">
-          <!-- svelte-ignore a11y-no-static-element-interactions -->
-          <div on:click={() => {setKey()}}>
-            <Button bg="secondary">Save</Button>
-          </div>
-        
-          <Dialog.Close>
-            <Button bg="error">Ignore for now</Button>
-          </Dialog.Close>
-      </div>
-    </Dialog.Content>
-  </Dialog.Portal>
-</Dialog.Root>
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -15,9 +15,5 @@
     <input type="text" class="h-input w-full border-none bg-background-alt rounded focus:outline-none p-2" placeholder="C:/Program Files (x86)/Steam/" bind:value={$steamPath}>
     <br>
     <br>
-    <p class="text-2xl" id="keypopup">Change Nexus Key</p>
-    <Button click={() => {
-        dialogOpened.set(true)
-    }} bg="secondary">Re-Open popup</Button>
 </div>
 
