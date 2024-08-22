ab63389e0f6b5522b808af271a70bad7a6234935
lu2000luk <lu2000luk@gmail.com>
2024-08-22 20:28:08 +0200

    Updated coming soon page

--- src/routes/creators/coming_soon/+page.svelte
+++ src/routes/creators/coming_soon/+page.svelte
@@ -1,3 +1,3 @@
-<h1>
-    Coming soon...
+<h1 class="flex justify-center w-full h-full flex-col items-center text-3xl">
+    <span class="p-6 rounded-lg bg-gradient-to-r from-violet-900 to-red-600">Coming soon...</span>
 </h1>
